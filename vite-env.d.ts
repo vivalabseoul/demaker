7fd0dbd41e2dc3b91075fb42cdbbb0f2e4a5eaef
stella8006 <stella.j.moon@gmail.com>
2025-11-17 10:27:27 +0900

    Fix TypeScript import.meta.env type errors

---
+++ vite-env.d.ts
@@ -0,0 +1,41 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL?: string;
+  readonly VITE_SUPABASE_PROJECT_ID?: string;
+  readonly VITE_SUPABASE_ANON_KEY?: string;
+  readonly VITE_BASE_PATH?: string;
+  readonly NODE_ENV: 'development' | 'production' | 'test';
+  readonly MODE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+// 이미지 파일 타입 정의
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
+
